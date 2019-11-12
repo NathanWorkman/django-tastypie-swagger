8241d401ad71d301750c36eb7dd017669571210d
Nathan Workman <nathancworkman@gmail.com>
2019-11-12 15:05:26 -0500

    Update swagger and update mapping

--- tastypie_swagger/urls.py
+++ tastypie_swagger/urls.py
@@ -1,9 +1,12 @@
 from django.conf.urls import url
 
-from .views import SwaggerView, ResourcesView, SchemaView
+from .views import (
+    SwaggerView, ResourcesView, SchemaView, SwaggerSpecs2View
+)
 
 urlpatterns = [
     url(r'^$', SwaggerView.as_view(), name='index'),
+    url(r'^specs/(swagger.json)?$', SwaggerSpecs2View.as_view(), name='specs'),
     url(r'^resources/$', ResourcesView.as_view(), name='resources'),
     url(r'^schema/(?P<resource>\S+)/$', SchemaView.as_view()),
     url(r'^schema/$', SchemaView.as_view(), name='schema'),
